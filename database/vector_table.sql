2c2dbc715c10f7c40d0f78cc74ba0ebbd39a522b
Nayeemuddinga <gulam.ahmed786@outlook.com>
2025-12-16 21:33:44 +0530

    Update vector_table.sql

--- database/vector_table.sql
+++ database/vector_table.sql
@@ -1,1 +1,7 @@
+CREATE TABLE document_vectors (
+    doc_id NUMBER GENERATED ALWAYS AS IDENTITY,
+    content CLOB,
+    embedding VECTOR(1024),
+    CONSTRAINT document_vectors_pk PRIMARY KEY (doc_id)
+);
 
